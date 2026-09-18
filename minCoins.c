3164ee5db612e75b4d766578542e7e3647b42dac
PPPPPPiii <may.march25lily@gmail.com>
2026-09-18 16:46:54 -0400

    Fix minCoins function to include nickels

--- minCoins.c
+++ minCoins.c
@@ -2,11 +2,12 @@
 #include<stdlib.h>
 
 int minCoins(int cents){
-    int quarters = cents / 25;
+      int quarters = cents / 25;
     int dime = (cents - quarters*25) / 10;
-    int pennies = cents - quarters*25 - dime*10;
+    int nickels = (cents - quarters*25 - dime*10) / 5;
+    int pennies = cents - quarters*25 - dime*10 - nickels*5;
 
-    int total = quarters + dime + pennies;
+    int total = quarters + dime + nickels + pennies;
 
     return total;
 }
