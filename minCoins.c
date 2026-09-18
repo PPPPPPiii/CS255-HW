1f630d83f3b8cce5c6a2e82451018ac9d8ec765c
PPPPPPiii <may.march25lily@gmail.com>
2026-09-18 16:44:49 -0400

    Refactor minCoins function to use integer cents

--- minCoins.c
+++ minCoins.c
@@ -1,13 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int minCoins(double input){
-    double cents = input / 100;
-    int quarters = cents / 0.25;
-    
-    int dime = (cents - quarters*0.25)/0.1;
-
-    double pennies = (cents - quarters*0.25 - dime*0.1)/0.01;
+int minCoins(int cents){
+    int quarters = cents / 25;
+    int dime = (cents - quarters*25) / 10;
+    int pennies = cents - quarters*25 - dime*10;
 
     int total = quarters + dime + pennies;
 
